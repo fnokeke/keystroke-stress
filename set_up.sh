d18583905cfdcaf75a419dc3d32b963096de0a8a
Fabian Okeke <fnokeke@gmail.com>
2015-05-06 13:33:05 -0400

    updated start up script for a cleaner opening of apps

--- set_up.sh
+++ set_up.sh
@@ -7,20 +7,22 @@ echo "*****************************************************"
 echo "Installing pykeycode...."
 git clone https://github.com/abarnert/pykeycode.git
 
-PYCODE_PATH="$(pwd)/pykeycode/"
+PROJECT_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+PYCODE_PATH="$PROJECT_PATH/pykeycode/"
 (cd $PYCODE_PATH; sudo python setup.py install)
 
-PROJECT_PATH="$(pwd)"
 ############### open accessibility ###############
 osascript $PROJECT_PATH/open_accessibility.scpt
 
 ############### start keystroke application ###############
-open $PROJECT_PATH/startup_keystroke.app
+ps aux | grep ninja_keyboard.app | grep -v grep | awk '{print $2}' | xargs kill -9
+ps aux | grep overall_logger.py | grep -v grep | awk '{print $2}' | xargs kill -9
+open $PROJECT_PATH/ninja_keyboard.app
 
 ############### start foreground application ###############
+ps aux | grep startup_foreground.app | grep -v grep | awk '{print $2}' | xargs kill -9
 open $PROJECT_PATH/startup_foreground.app
 
 ############### start PAM/EMA application ###############
+ps aux | grep pam_and_self_report.app | grep -v grep | awk '{print $2}' | xargs kill -9
 open $PROJECT_PATH/pam_and_self_report.app
-
-############### export command to visualize data  ###############
